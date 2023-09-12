737dc691231757796d809a9fba27ba1405e726ff
Forvard <ficaliighvdrb@gmail.com>
2023-09-12 09:43:37 +0300

    0.0.3

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Strategies.ROUTE import main, wallet, search_balance
 import config as cng
 from Utils.EVMutils import EVM
 from multiprocessing.dummy import Pool
+from Log.Loging import log
 
 
 def zksync_tx():
@@ -17,5 +18,26 @@ def zksync_tx():
         search_balance(value)
 
 
+def zksync_tx_():
+    private = wallet()
+    if cng.CHECK_GWEI:
+        EVM.check_gwei()
+
+    def work(private_key, address_to):
+        EVM.delay_start()
+        main(private_key, address_to)
+    try:
+        with Pool(cng.THREAD) as pols:
+            pols.map(lambda func: work(func[0], func[1]), private)
+    except BaseException as error:
+        log().error(error)
+
+
 if __name__ == '__main__':
-    zksync_tx()
+    print("""
+                 1 - Очередь
+                 2 - Потоки
+    """)
+    works = input('Какой модуль крутим: ')
+    func = {1: zksync_tx, 2: zksync_tx_}
+    func[int(works)]()
